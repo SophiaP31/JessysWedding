08a05b4cf3560c82f34a5ceb1aac723cc10ad4b4
sophiap31 <sap8ht@virginia.edu>
2019-03-26 22:54:57 -0400

    responsive navigation bar, photo gallery, etc

---
+++ README.txt
@@ -0,0 +1,17 @@
+To do:
+
+Overall:
+Make responsive nagivation Bar
+clean up all the style tags and make them organized!! (give everything an ID or Class)
+
+TRANSPORTATION:
+-add a pretty picture and TBD
+WEDDING DETAILS:
+-add a pretty picture and wedding details text
+REGISTRY:
+-add a pretty picture?
+MEET THE WEDDING PARTY:
+-add bios and pictures
+OUR STORY:
+-add a picture (ring pic?)
+-add story text
--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 11, 2019 05:00:00").getTime();
+var countDownDate = new Date("Oct 13, 2019 05:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get todays date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
   document.getElementById("countdown").innerHTML = days + "days " + hours + "hours "
   + minutes + "minutes " + seconds + "seconds ";
-    
-  // If the count down is over, write some text 
+
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "Today's the Day!";
   }
 }, 1000);
+
+//responsive nagivation bar
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
