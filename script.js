64a07383b905c09a012c14ad6e7a28903e02da06
sophiap31 <sap8ht@virginia.edu>
2019-10-14 13:41:23 -0400

    fixed countdown

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ var x = setInterval(function() {
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "Today's the Day!";
+    document.getElementById("countdown").innerHTML = "Jessy & Matt";
+    document.getElementById("until").innerHTML = "10.13.19";
   }
 }, 1000);
 
